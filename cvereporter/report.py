241b00d88b16361327c69e2232a4dbec76b875d4
Daniel Scanteianu <dscanteianu@bloomberg.net>
2024-04-25 12:50:27 +0100

    fix flake 8: :

--- cvereporter/report.py
+++ cvereporter/report.py
@@ -92,7 +92,7 @@ def sbom_creation_test():
     affects1 = BomTarget(ref="temurin-vdr")
     vuln1.affects.add(affects1)
 
-    my_json_outputter: "JsonOutputter" = JsonV1Dot4(bom)
+    my_json_outputter = JsonV1Dot4(bom)
     serialized_json = my_json_outputter.output_as_string(indent=2)
     print("\n\n\n")
     print(serialized_json)
